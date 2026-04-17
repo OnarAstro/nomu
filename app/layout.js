9525706eeddf2d85fbb95745a9c8391a58d56afb
Omar Atef <126633818+OnarAstro@users.noreply.github.com>
2026-04-17 15:38:33 +0200

    mnl

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,15 @@ import "./globals.css";
 import Head from "next/head";
 const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
-export const metadata = {
-  title: "Nomu - development",
-  description: "Precision development for your digital vision.",
-  themeColor: "#000000",
-  icons: {
-    icon: "/logo.png",
-  },
-  manifest: "/manifest.json",
-};
+// export const metadata = {
+//   title: "Nomu - development",
+//   description: "Precision development for your digital vision.",
+//   themeColor: "#000000",
+//   icons: {
+//     icon: "/logo.png",
+//   },
+//   manifest: "/manifest.json",
+// };
 
 
 export default function RootLayout({ children }) {
@@ -19,11 +19,15 @@ export default function RootLayout({ children }) {
     <>
 
     <Head>
+
+      <title>Nomu - development</title>
+      <meta name="description" content="Precision development for your digital vision." />
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+      <link rel="icon" type="image/svg+xml" href="/logo.png" />
       <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#000000" />
-      
+
     </Head>
 
       <html lang="en">
